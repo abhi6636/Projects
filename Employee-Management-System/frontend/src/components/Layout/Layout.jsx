c9f83ff4cbe2a07e301810d3b8d4bd485abf631e
Abhinand <abhinandm94@gmail.com>
2023-08-19 00:33:06 +0530

    19082023

---
+++ Employee-Management-System/frontend/src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./../header/Header";
+import Routers from "./../../Routers/Routers";
+import Footer from "../Footer/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Routers />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
